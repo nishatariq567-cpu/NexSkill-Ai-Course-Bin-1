8f8103d2213047d2c0c8009513335c07e21f1f45
nishatariq567-cpu <nishatariq567@gmail.com>
2026-06-24 18:37:30 +0500

    my code

---
+++ practices data structure tuple.py
@@ -0,0 +1,18 @@
+booktuple=(445,"think and grow","nipalian hill",44.9)
+print(booktuple)
+
+print(type(booktuple))
+print(len(booktuple))
+
+for i in booktuple:
+    print(i)
+
+print=(booktuple[2])
+print=(type(booktuple[2]))
+
+
+
+
+
+
+

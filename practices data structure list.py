b2b564f7d0c9532242c9893f383308a6b5f7169a
nishatariq567-cpu <nishatariq567@gmail.com>
2026-06-24 18:22:40 +0500

    my code

---
+++ practices data structure list.py
@@ -0,0 +1,36 @@
+book list=[445,"think and grow", "nipalian hill", 44.9]
+print(booklist)
+print(type(booklist))
+print=len(booklist)
+
+for i in booklist:
+    print(i)
+
+print=(booklist[2])
+
+print=(type(booklist[2]))
+
+price=(type(booklist[3]))
+
+booklist.append("abc publisher")
+print(booklist)
+
+booklist.insert(1,"1947")
+print(booklist)
+
+booklist.remove(44.9)
+print(booklist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

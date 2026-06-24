8d40e54d09c483878d6d73ff332eacca76b17cc4
nishatariq567-cpu <nishatariq567@gmail.com>
2026-06-24 18:47:22 +0500

    my code

---
+++ practices data structure set.py
@@ -0,0 +1,17 @@
+bookset={"book1":"the alchemist":"book2"}
+print(bookset)
+print(type(bookset))
+print(len(bookset))
+
+for i in bookset:
+    print(i)
+bookset.add(bookset3)
+print(bookset)
+
+bookset.remove(bookset2)
+print(bookset)
+
+
+
+
+

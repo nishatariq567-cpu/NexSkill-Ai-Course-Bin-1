275d22e3ad4f8695425a1f414cd7c39699f06962
nishatariq567-cpu <nishatariq567@gmail.com>
2026-06-29 18:32:15 +0500

    my first code

---
+++ practice class 29-6-2026.py
@@ -0,0 +1,28 @@
+print("welcom to practice class 29-6-2026")
+mobilename=input("pleace enter your mobile name:")
+print(mobilename)
+print(type(mobilename))
+print(len(mobilename))
+
+for i in mobilename:
+    print=(i)
+
+
+# my first code ends here
+
+mobileprice=float(input("please enter your mobile price:"))
+print(mobileprice)
+print(type(mobileprice))
+
+if mobileprice>5:
+    print("grater 5")
+else mobileprice<5:
+    print("less than 5")
+if else mobileprice<3:
+    print("less then 3")  
+else:
+    print("what ever..")
+
+
+
+
---
+++ string practice class 29-6-2026.py
@@ -0,0 +1,19 @@
+mobileinfo=input("please enter your mobile info:")
+print(mobileinfo)
+print(type(mobileinfo))
+print(len(mobileinfo))
+
+for i in mobileinfo:
+    print=(i)
+
+first5=mobileinfo[:5:1]
+print(first5)
+first5B=mobileinfo[0:5:1]
+print(first5B)
+first10To115=mobileInfo[ 10:15:1]
+print(first10To115)
+firstTillEnd=mobileInfo[ 0::1]
+print(firstTillEnd)
+
+
+
